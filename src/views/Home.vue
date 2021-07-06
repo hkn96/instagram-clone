49a5840bdddf4bd6bc559d6f6cf7121a1d8d2183
Hakan Ünlü <unluhak@outlook.com>
2021-07-07 01:41:21 +0200

    vue.svg-loader installed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,33 @@
 <template lang="">
   <div class="home">
     <h1>Home</h1>
+    <CustomText class="a1">Hallo Welt</CustomText>
+    <CustomText class="a2" tag="p" size="xsmall">Hallo Welt</CustomText>
+    <CustomText class="a3" tag="p" size="xxsmall">Hallo Welt</CustomText>
+    <CustomText class="a4" tag="p" size="normal">Hallo Welt</CustomText>
   </div>
 </template>
 
 <script>
+import CustomText from '../components/CustomText.vue';
 // @ is an alias to /src
 
 export default {
   name: 'Home',
+  components: {
+    CustomText,
+  },
 };
 </script>
 
 <style scoped>
-h1 {
-  color: red;
-
-  @media (--t) {
-    color: blue;
-  }
+.a1 {
+  color: rgb(var(--ba8));
+}
+.a2 {
+  color: var(--b3f);
+}
+.a3 {
+  color: var(--ba8);
 }
 </style>
