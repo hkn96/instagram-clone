f0d67c1e050bd9a538d3a9202675b98645a01449
Hakan Ünlü <unluhak@outlook.com>
2021-07-07 00:43:36 +0200

    postcss config erledigt ("postcss-custom-media": "^7.0.0",     "postcss-nested": "^4.2.3",
    ------------------------
    
    npm i -D postcss-custom-media@7.0.0 postcss-nested@4.2.3)

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-<template>
+<template lang="">
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
@@ -8,25 +8,4 @@
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<style></style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,4 +1,4 @@
-<template>
+<template lang="">
   <div class="about">
     <h1>This is an about page</h1>
   </div>
